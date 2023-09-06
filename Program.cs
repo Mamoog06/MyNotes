3029ad60a9d71a52c42e1446dff5ae7feb651390
Marcus_Hansenberg <marc453b@mail.hansenberg.dk>
2023-09-06 12:49:25 +0200

    Har simplificeret bogen

--- Program.cs
+++ Program.cs
@@ -1,26 +1,12 @@
-﻿Book bog = new Book("Programming notes");
+﻿Book bog = new Book("Programming notes", new List<Chapter>(){
+    new Chapter("Variabler", new List<Page>(){
+        new Page(MyNotes.VariablesPageOne),
+        new Page(MyNotes.VariablesPageTwo),
+    }),
+    new Chapter("Conditioner", new List<Page>(){
+        new Page(MyNotes.conditionalsPageOne),
+    }),
+});
 
-List<Page> pages = new List<Page>();
-Page variablesPage1 = new Page(MyNotes.VariablesPageOne);
-Page variablesPage2 = new Page(MyNotes.VariablesPageTwo);
-Page page3 = new Page("side 3");
+bog.Start();
 
-pages.Add(variablesPage1);
-pages.Add(variablesPage2);
-
-Chapter variableChapter = new Chapter("Variabler");
-variableChapter.Pages = pages;
-Chapter chap2 = new Chapter("kapitel 2", pages);
-
-variablesPage1.Read();
-chap2.Read();
-
-List<Chapter> chapters = new List<Chapter>();
-chapters.Add(variableChapter);
-chapters.Add(chap2);
-
-
-
-
-//pause
-Console.ReadLine();
