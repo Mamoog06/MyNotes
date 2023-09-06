dc8e47dc1e74bcc18c3084df4c4cac15bb20a9cd
Marcus_Hansenberg <marc453b@mail.hansenberg.dk>
2023-09-06 11:20:28 +0200

    Ændret på Variables page 2

--- MyNotes.cs
+++ MyNotes.cs
@@ -14,14 +14,31 @@ Her er nogle eksempler:
 
     public static string VariablesPageTwo =
 @"...
-So often used without consideration for its magnificence, the humble 'variable' is the heart of computation. 
-It can be used to store and manipulate data, and is the backbone of all computer programs. 
-The 'variable' is a named container for a value, and can be of different types. 
-The type of a variable determines what kind of value it can hold, and what kind of operations can be performed on it. 
-There are many different types of variables, but the most common ones are 'int', 'string', 'float', and 'bool'. 
-An 'int' is a whole number, like 1, 2, 3, 4, 5, 6, 7, 8, 9, 0. 
-A 'string' is a sequence of characters, like ""Hello World"", ""I am a string"", and ""C# is awesome"". 
-A 'float' is a decimal number, like 1.234, 5.678, and 9.101112. 
-A 'bool' is a true or false value, like true and false. ";
+De basale datetyper, som du har nævnt (int, float, string, bool), bruges til at repræsentere forskellige typer af data i programmeringssprog.
+Her er en forklaring på hver disse datatyper.
+'variablen¨ er en navngivet beholder af en værdi, og kan være af forskllige typer.
+Type af variablen bestemme hvilken slags værdi den kan holde, eller hvilke slags operationer kan blive udført.
+Der er mange forskellige typer af variabler, men de mest kendte er 'int', 'string', 'float', and 'bool'.
+
+'int' står for 'integere' hvilket betyder heltal på dansk.
+Denne datatype bruges til at repræsentere hele tal uden decimaler.
+et eksempel på heltal: -4, 0, 45, 3939399
+eksempel på 'int': x = 5
+
+'float' står for 'floating-point' hvilket betyder tal med decimaler på dansk.
+DEnne datatype bruges til at repræsentere tal med decimaler.
+Eksempler på decimaltal: -3.492, 6,129, 93149,123
+Eksempel på 'float': pi = 3.14159
+
+'string' bruges til at repræsentere tekst eller en sekvens af tegn.
+Tekststrengen skal normalt omgives af enten ekelt- eller dobbelcitationstegn.
+Eksempel på en tekststreng: 'Hello world!'
+Eksempel på en 'string': name = 'Marcus'
+
+'bool' står for 'boolean' hvilket betyder sandt eller falsk på dansk.
+Denne datatype bruges til at repræsentere to mulige værdiger: sandt (True) eller falsk (False).
+Den bruges ofte til at udtrykke betingelser i programmer, hvor noget enten er sandt eller falsk.
+Eksempler på brug: 'True', 'False'
+Eksempel på 'bool': is_sunny = True ";
 
 }
